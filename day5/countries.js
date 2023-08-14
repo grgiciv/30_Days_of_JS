de2cac899642ce00f6a018d2b75650fb4cf22b9c
Ivan Grgić <ivan.grgic5@gmail.com>
2023-08-14 11:24:40 +0200

    minor changes

--- day5/countries.js
+++ day5/countries.js
@@ -1,14 +1,14 @@
 const countries = [
-    'Albania',
-    'Bolivia',
-    'Canada',
-    'Denmark',
-    'Ethiopia',
-    'Finland',
-    'Germany',
-    'Hungary',
-    'Ireland',
-    'Japan',
-    'Kenya'
-  ];
-  export { countries }
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+];
+export { countries };
